7f3a5137c433ce8cb6e988d8c4f3133e0a0b4bf2
mathiskakal <mathis.kakal@algosup.com>
2022-11-02 19:35:48 +0100

    Add user input

--- simpleCLI/main.go
+++ simpleCLI/main.go
@@ -5,12 +5,29 @@ import (
 )
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	// ask user for their first name, last name, email
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+	fmt.Println("How many tickets do you want ?")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive an email confirmation at %vn", firstName, lastName, userTickets, email)
 }
