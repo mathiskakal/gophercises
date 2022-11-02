c373f55f287068a61bf4bd36975f9e5798b4290a
mathiskakal <mathis.kakal@algosup.com>
2022-11-02 17:24:49 +0100

    n3

--- simpleCLI/main.go
+++ simpleCLI/main.go
@@ -5,7 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
 
 }
