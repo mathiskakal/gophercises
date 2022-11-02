05746f5b690e6f6b5c550567c18957d23c6cf3de
mathiskakal <mathis.kakal@algosup.com>
2022-11-02 19:13:57 +0100

    printf refactor

--- simpleCLI/main.go
+++ simpleCLI/main.go
@@ -9,8 +9,8 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 }
