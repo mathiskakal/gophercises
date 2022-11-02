ae92987c02c3167fd263fc633f4e03871761e0b7
mathiskakal <mathis.kakal@algosup.com>
2022-11-02 17:22:15 +0100

    2 	modified:   simpleCLI/main.go

--- simpleCLI/main.go
+++ simpleCLI/main.go
@@ -8,6 +8,4 @@ func main() {
 	fmt.Println("Welcome to our conference booking application")
 	fmt.Println("Get your tickets here to attend")
 
-	var conferenceName = "Go Conference"
-	fmt.Println(conferenceName)
 }
