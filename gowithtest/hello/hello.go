808e84fa2bc0f3bcb1620430e1e4dbfe7ddb4fd0
mathiskakal <mathis.kakal@algosup.com>
2022-11-03 20:55:25 +0100

    finished ex1

--- gowithtest/hello/hello.go
+++ gowithtest/hello/hello.go
@@ -1,17 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const germanHelloPrefix = "Halo, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		return englishHelloPrefix + "World"
 	}
 
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	case "German":
+		prefix = germanHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- gowithtest/hello/hello_test.go
+++ gowithtest/hello/hello_test.go
@@ -3,16 +3,40 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	// Testing for a name
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
+	// Testing for an empty name
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	// Testing for Spanish
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+	// Testing for French
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Marc", "French")
+		want := "Bonjour, Marc"
+
+		assertCorrectMessage(t, got, want)
+	})
+	// Testing for German
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Adolf", "German")
+		want := "Halo, Adolf"
 
 		assertCorrectMessage(t, got, want)
 	})
@@ -26,10 +50,5 @@ func assertCorrectMessage(t testing.TB, got, want string) {
 }
 
 /*
-func assertCorrectMessage(t testing.TB, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
-*/
+
+ */
