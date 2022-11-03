9dd87cf779c5e88fd9190da40e3542f8a979b43b
mathiskakal <mathis.kakal@algosup.com>
2022-11-03 13:14:57 +0100

    split files with helper functions logic

--- simpleCLI/main.go
+++ simpleCLI/main.go
@@ -99,16 +99,3 @@ func getFirstNames() []string {
 	}
 	return firstNames
 }
-
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	// make sure that input name and last name contain at least two characters. Will be true if condition is met
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-
-	// same thing for email validation, we make sure it contains @, will return true if it does
-	isValidEmail := strings.Contains(email, "@")
-
-	// validate that both user requested a positive whole number of tickets and that it doesn't exceed the amount left.
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-
-	return isValidName, isValidEmail, isValidTicketNumber
-}
