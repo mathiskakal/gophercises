4a131c7bcddc84f05d20b062091fc2fb971eac3a
mathiskakal <mathis.kakal@algosup.com>
2022-11-04 12:08:23 +0100

    finished arrays&slice ex.

--- gowithtest/arraysAndSlices/sum_test.go
+++ gowithtest/arraysAndSlices/sum_test.go
@@ -53,14 +53,22 @@ func TestSumAllTails(t *testing.T) {
 */
 
 func TestSumAllTails(t *testing.T) {
+	// Scoped Helper Function
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+
+	// Sub Tests
 	t.Run("make the sums of some slices", func(t *testing.T) {
 
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v, want %v", got, want)
-		}
+		checkSums(t, got, want)
+
 	})
 
 	t.Run("make the sums of some slices", func(t *testing.T) {
@@ -68,8 +76,6 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v, want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }
