391866f124b141a5312d725bbfab8bebcec94270
mathiskakal <mathis.kakal@algosup.com>
2022-11-04 11:32:11 +0100

    sumAllTails variant #1

--- gowithtest/arraysAndSlices/sum.go
+++ gowithtest/arraysAndSlices/sum.go
@@ -17,10 +17,20 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) []int {
+/*
+func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+*/
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
 	}
 	return sums
 }
