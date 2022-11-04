55b4f122098782683012714865cb0d73a3b81a8b
mathiskakal <mathis.kakal@algosup.com>
2022-11-04 08:57:07 +0100

    arrays&slices halfway done

--- gowithtest/arraysAndSlices/sum.go
+++ gowithtest/arraysAndSlices/sum.go
@@ -1,9 +1,18 @@
 package main
 
-func Sum(numbers [5]int) int {
-	return 0
-}
+// func Sum(numbers [5]int) int {
+// 	sum := 0
+// 	for _, number := range numbers {
+// 		sum += number
+// 	}
+// 	return sum
 
-func main() {
+// }
 
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
 }
