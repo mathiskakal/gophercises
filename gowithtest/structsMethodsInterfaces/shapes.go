7d317f363bd0ab1362740e451053041302376552
mathiskakal <mathis.kakal@algosup.com>
2022-11-07 16:27:33 +0100

    introduce shape interface and refactor test for it

--- gowithtest/structsMethodsInterfaces/shapes.go
+++ gowithtest/structsMethodsInterfaces/shapes.go
@@ -1,9 +1,48 @@
 package main
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+/*ஐఴஐ๑ஐఴஐஐஐఴஐ๑ஐఴஐஐஐఴ
+಄ะ Types & Variables ~~ ะ಄
+ஐஐळஐ๑ஐळஐஐஐळஐ๑ஐळஐஐஐळ*/
+
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+type Circle struct {
+	Radius float64
 }
+
+type Shape interface {
+	Area() float64
+}
+
+/*ஐఴஐ๑ஐఴஐஐஐఴஐ๑ஐఴஐஐஐఴ
+಄ะ Methods ~~ ะ಄
+ஐஐळஐ๑ஐळஐஐஐळஐ๑ஐळஐஐஐळ*/
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+/*ஐఴஐ๑ஐఴஐஐஐఴஐ๑ஐఴஐஐஐఴ
+಄ะ Functions ~~ ะ಄
+ஐஐळஐ๑ஐळஐஐஐळஐ๑ஐळஐஐஐळ*/
+
+// Write Here
+
+/*
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+*/
