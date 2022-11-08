9e33b831a99aeb4f97ccdda76ff42a8baf4db6a8
mathiskakal <mathis.kakal@algosup.com>
2022-11-08 13:17:51 +0100

    halfway commit

--- gowithtest/maps/dictionary.go
+++ gowithtest/maps/dictionary.go
@@ -1,5 +1,41 @@
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import "errors"
+
+/*ஐఴஐ๑ஐఴஐஐஐఴஐ๑ஐఴஐஐஐఴ
+಄ะ types & variables ะ಄
+ஐஐळஐ๑ஐळஐஐஐळஐ๑ஐळஐஐஐळ*/
+
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
+
+type Dictionary map[string]string
+
+/*ஐఴஐ๑ஐఴஐஐஐఴஐ๑ஐఴஐஐஐఴ
+಄ะ methods ะ಄
+ஐஐळஐ๑ஐळஐஐஐळஐ๑ஐळஐஐஐळ*/
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
 }
